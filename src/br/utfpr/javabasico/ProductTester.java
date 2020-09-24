a0d80749db3f9476e9959d0aa29c8ec0f9ddf395
Edilson do Nascimento <edilsonjoper@gmail.com>
2020-09-24 15:08:14 -0300

    Seção 6: Criando um Projeto de Inventário Usando loops (Seções 5.2 e 6.1) - Como tratar exceções (Seções 6.2) - Usando instruções if (Seção 5.1) - Arrays de objetos (Seção 6.1)

--- src/br/utfpr/javabasico/ProductTester.java
+++ src/br/utfpr/javabasico/ProductTester.java
@@ -3,64 +3,57 @@ package br.utfpr.javabasico;
 import java.util.Scanner;
 
 public class ProductTester {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner in = new Scanner(System.in);
 		long tempNumber;
 		String tempName;
 		int tempQty;
 		double tempPrice;
-		
-		System.out.print("Entre com o código do Produto: ");
-		tempNumber = in.nextLong();
-		System.out.print("Entre com a descrição do Produto: ");
-		tempName = in.next();
-		System.out.print("Entre com a quantidade em estoque do Produto: ");
-		tempQty = in.nextInt();
-		System.out.print("Entre com o preço do Produto: ");
-		tempPrice = in.nextDouble();
-		Produto p1 = new Produto(tempNumber, tempName, tempQty, tempPrice);
-		System.out.println(p1.toString());
-		
-		//quebra de linha
-		System.out.println();
-		//limpar buffer.
-		in.nextLine();
-		
-		System.out.print("Entre com o código do Produto: ");
-		tempNumber = in.nextLong();
-		System.out.print("Entre com a descrição do Produto: ");
-		tempName = in.next();
-		System.out.print("Entre com a quantidade em estoque do Produto: ");
-		tempQty = in.nextInt();
-		System.out.print("Entre com o preço do Produto: ");
-		tempPrice = in.nextDouble();
-		System.out.print("Entre com o preço do Produto: ");
-		tempPrice = in.nextDouble();
-		System.out.println();
-		Produto p2 = new Produto(tempNumber, tempName, tempQty, tempPrice);		
-		System.out.println(p2.toString());
-	    
-		in.close();
-		
-		Produto p3 = new Produto(30, "Mesa", 5, 780.6);
-		Produto p4 = new Produto(40, "Sofa", 7, 3980.27);
-		Produto p5 = new Produto(50, "Telvisor", 100, 5000.99);
-		Produto p6 = new Produto(60, "Cama", 10, 1748.98);
-		
-		//Mostra produtos com toString
-		System.out.println(p1.toString());
-		System.out.println(p2.toString());
-		System.out.println(p3.toString());
-		System.out.println(p4.toString());
-		System.out.println(p5.toString());
-		System.out.println("Tirando item:  " + p6.getNumeroItem() + " do estoque...");
-	    p6.setAtivo(false);
-		System.out.println(p6.toString());		
-	    
+		int maxSize = -1;
 
-				
+		System.out.println("Insira o número de produtos que gostaria de adicionar\r\n"
+				+ " Insira 0 (zero) se não quiser adicionar produtos: ");
+
+		while (maxSize < 0) {
+			try {
+				maxSize = in.nextInt();
+				if (maxSize == 0) {
+					break;
+				} else if (maxSize < 0) {
+					System.out.println("Valor incorreto inserido");
+					in.nextLine();
+				}
+			} catch (Exception e) {
+				System.out.println("Tipo incorreto de dados inserido!");
+				in.nextLine();
+			}
+		}
+
+		if (maxSize == 0) {
+			System.out.println("Não ha produtos!");
+		} else {
+			Produto[] produtos = new Produto[maxSize];
+			for (int i = 0; i < maxSize; i++) {
+				in.hasNextLine();
+				System.out.println();
+				System.out.print("Entre com o código do Produto: ");
+				tempNumber = in.nextLong();
+				System.out.print("Entre com a descrição do Produto: ");
+				tempName = in.next();
+				System.out.print("Entre com a quantidade em estoque do Produto: ");
+				tempQty = in.nextInt();
+				System.out.print("Entre com o preço do Produto: ");
+				tempPrice = in.nextDouble();
+				produtos[i] = new Produto(tempNumber, tempName, tempQty, tempPrice);
+			}
+
+			for (Produto produto : produtos) {
+				System.out.println(produto);
+			}
+		}
+		in.close();
 	}
 
 }
